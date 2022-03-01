dd75fb0ea231a672163b25079455712a6e30b04f
Alex Rybachev <rybachev.as@gmail.com>
2022-03-01 21:43:43 +0400

    1) add outlets and ibactions 2) add function for changing traffic light

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Views Properties
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    
+    // MARK: - Button Properties
+    @IBOutlet var lightButton: UIButton!
+    
+    // MARK: - Lights Properties
+    var isRedLightOn = true
+    var isYellowLightOn = false
+    var isGreenLightOn = false
+    var lightColor = LightColor.red
+    
+    // MARK: - Initial
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupLightViews(for: [redLightView, yellowLightView, greenLightView])
+        lightButton.configuration = setupButton(with: "START")
+        
     }
+    
+    // MARK: - IBActions
+    @IBAction func changeTraficLight() {
+        
+        lightButton.configuration = setupButton(with: "NEXT")
+        
+        redLightView.backgroundColor = .red.withAlphaComponent(isRedLightOn ? 1 : 0.3)
+        yellowLightView.backgroundColor = .yellow.withAlphaComponent(isYellowLightOn ? 1 : 0.3)
+        greenLightView.backgroundColor = .green.withAlphaComponent(isGreenLightOn ? 1 : 0.3)
 
+        switch lightColor {
+        case .red:
+            isRedLightOn = false
+            isYellowLightOn = true
+            lightColor = .yellow
+        case .yellow:
+            isYellowLightOn = false
+            isGreenLightOn = true
+            lightColor = .green
+        case .green:
+            isGreenLightOn = false
+            isRedLightOn = true
+            lightColor = .red
+        }
+    }
+    
+    // MARK: - Settings
+    private func setupLightViews(for views: [UIView]) {
+        redLightView.backgroundColor = .red.withAlphaComponent(0.3)
+        yellowLightView.backgroundColor = .yellow.withAlphaComponent(0.3)
+        greenLightView.backgroundColor = .green.withAlphaComponent(0.3)
+        
+        for view in views {
+            view.layer.cornerRadius = view.frame.width / 2
+        }
+    }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = .systemBlue
+        buttonConfiguration.cornerStyle = .large
+        buttonConfiguration.title = title
+        buttonConfiguration.attributedTitle?.foregroundColor = .white
+        buttonConfiguration.attributedTitle?.font = .boldSystemFont(ofSize: 32)
+        
+        return buttonConfiguration
+    }
+}
 
+// MARK: - Extension
+extension ViewController {
+    enum LightColor {
+        case red
+        case yellow
+        case green
+    }
 }
 
